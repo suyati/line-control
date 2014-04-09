f20e51265c5faeceb58e7d85a055387dfe12e8c1
Arun V B <arunvazhavila@gmail.com>
2014-04-09 12:59:59 +0530

    Update editor.css

--- editor.css
+++ editor.css
@@ -1,6 +1,6 @@
 /*!
  * http://suyati.github.io/line-control
- * LineControl 1.0.0
+ * LineControl 0.1.0
  * Copyright (C) 2014, Suyati Technologies
  * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
 This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
